69d9d3fe3e994b7fcee0421cca8f1702f7478539
kimbeomjoon99 <tnldls99@naver.com>
2024-08-06 13:47:29 +0900

    docs: db 서버명 변경

--- src/main/java/com/myinvestment/diary/controller/DiaryController.java
+++ src/main/java/com/myinvestment/diary/controller/DiaryController.java
@@ -14,26 +14,31 @@ public class DiaryController {
     private final DiaryService diaryService;
 
     @GetMapping("/diary")
+    @CrossOrigin("*")
     public List<DiaryResponseDto> getAllDiaries(@RequestParam String date){
         return diaryService.getAllDiaries(date);
     }
 
     @GetMapping("/diary/{id}")
+    @CrossOrigin("*")
     public DiaryResponseDto findDiaryById(@PathVariable int id){
         return diaryService.findDiaryById(id);
     }
 
     @PostMapping("/diary/new")
+    @CrossOrigin("*")
     public DiaryPkResponseDto createDiary(@RequestBody DiaryCreationRequestDto dto){
         return diaryService.createDiary(dto);
     }
 
     @GetMapping("/diary/portfolio")
+    @CrossOrigin("*")
     public List<PortfolioResponseDto> getPortfolio(@RequestParam String userID){
         return diaryService.getPortfolio(userID);
     }
 
     @PostMapping("/diary/update")
+    @CrossOrigin("*")
     public void modifyDiary(@RequestBody DiaryModifyRequestDto dto){
         diaryService.modifyDiary(dto);
     }
--- src/main/java/com/myinvestment/openai/controller/AIController.java
+++ src/main/java/com/myinvestment/openai/controller/AIController.java
@@ -4,6 +4,7 @@ import com.myinvestment.openai.dto.AIResponseDto;
 import com.myinvestment.openai.service.AIService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,13 @@ public class AIController {
     private final AIService service;
 
     @GetMapping("/diary/feedback/openai")
+    @CrossOrigin("*")
     public AIResponseDto getAIFeedback(@RequestParam String userId){
         return service.getAIFeedback(userId);
     }
 
     @GetMapping("/diary/strategy/openai")
+    @CrossOrigin("*")
     public AIResponseDto getAIStrategy(@RequestParam String userId){
         return service.getAIStrategy(userId);
     }
